fc361008af2529256c979ed0c1b18ba4865d04f7
hadrien <hadrien.stoppolani@etu.univ-amu.fr>
2022-01-29 13:21:22 +0100

    implements methods addToFavorite and isFavorite in Favorite

--- src/main/java/app/foodapp/model/Favorite.java
+++ src/main/java/app/foodapp/model/Favorite.java
@@ -3,10 +3,21 @@ package app.foodapp.model;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Favorite {
     private List<RecipeTmp> favorites;
 
     public Favorite(){
         favorites = new ArrayList();
     }
+
+    public void addToFavorite(RecipeTmp recipe){
+        if(!isFavorite(recipe))
+            favorites.add(recipe);
+    }
+
+    private boolean isFavorite(RecipeTmp recipe){
+        return favorites.contains(recipe);
+    }
+
 }
