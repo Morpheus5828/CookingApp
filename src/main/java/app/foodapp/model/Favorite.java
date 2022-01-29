9dce0e77bc6d006b57cf93d4c456d9c5f7fab854
hadrien <hadrien.stoppolani@etu.univ-amu.fr>
2022-01-29 13:18:03 +0100

    implements class Favorite

---
+++ src/main/java/app/foodapp/model/Favorite.java
@@ -0,0 +1,12 @@
+package app.foodapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Favorite {
+    private List<RecipeTmp> favorites;
+
+    public Favorite(){
+        favorites = new ArrayList();
+    }
+}
