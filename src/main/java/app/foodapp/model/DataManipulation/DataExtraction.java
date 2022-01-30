cbb6b152d2534a2c9953c132228f7cadd171a5a4
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-01-30 20:25:18 +0100

    End of DataExtraction class

--- src/main/java/app/foodapp/model/DataManipulation/DataExtraction.java
+++ src/main/java/app/foodapp/model/DataManipulation/DataExtraction.java
@@ -11,15 +11,14 @@ import java.util.Map;
 
 public class DataExtraction {
     // We lit attributes we need to display
-    private String ingredientTitle;
-    private String ingredientImage;
-    private String usedIngredientCount;
-    private String missedIngredient;
-    private int numberOfLikes;
+    private String title;
+    private String image;
+    private String usedIngredient;
+    private int cookingTime;
+    private int serving;
     private Gson gson;
 
     public DataExtraction(String dataIngredientText) {
-        System.out.println(dataIngredientText);
         // JSON Array
         /*JSONArray jsonArray = new JSONArray(dataIngredientText);
         JSONObject jobj = jsonArray.getJSONObject(0);
@@ -48,38 +47,13 @@ public class DataExtraction {
         for(int i = 0; i < jsonArray.length(); i++) {
             JSONObject jj = jsonArray.getJSONObject(i);
             Map mapDeTest = gson.fromJson(String.valueOf(jj), Map.class);
+            System.out.println(mapDeTest.get("name"));
+            System.out.println(mapDeTest.get("originalName"));
             System.out.println(mapDeTest.get("aisle"));
+            System.out.println(mapDeTest.get("unit"));
+// amount for us et unityshort et aussi en metrics
+
+
         }
-
-
-
-
-        JSONObject jj = jsonArray.getJSONObject(0);
-        System.out.println(jsonArray.length());
-
-        gson = new Gson();
-        Map mapDeTest = gson.fromJson(String.valueOf(jj), Map.class);
-
-
-
-
-
-
-
-        /*
-
-        JSONObject jobj = jsonArray.getJSONObject(0);
-        Map test = gson.fromJson(String.valueOf(jobj), Map.class);
-        */
-
-
-
-
-
-
-
-
-
-
     }
 }
