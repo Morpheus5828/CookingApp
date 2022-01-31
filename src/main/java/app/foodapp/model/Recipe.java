b3adc3836b97b83ad9c57cd22d62ec37203a5abe
ChloeButtigieg <chloe.buttigieg12@gmail.com>
2022-01-31 14:14:00 +0100

    add fields and constructor to Recipe class

--- src/main/java/app/foodapp/model/Recipe.java
+++ src/main/java/app/foodapp/model/Recipe.java
@@ -1,4 +1,17 @@
 package app.foodapp.model;
 
 public class Recipe {
+    private int id;
+    private String image;
+    private String title;
+    private int servings;
+    private int cookingTime;
+
+    public Recipe (final int id, final String image, final String title, final int servings, final int cookingTime) {
+        this.id = id;
+        this.image = image;
+        this.title = title;
+        this.servings = servings;
+        this.cookingTime = cookingTime;
+    }
 }
