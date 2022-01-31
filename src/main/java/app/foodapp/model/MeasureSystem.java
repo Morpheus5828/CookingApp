e353b87aba4e46a16b91c473f5af18e83385acca
ChloeButtigieg <chloe.buttigieg12@gmail.com>
2022-01-31 21:20:31 +0100

    change location of file MeasureSystem

--- src/main/java/app/foodapp/model/MeasureSystem.java
+++ src/main/java/app/foodapp/model/MeasureSystem.java
@@ -1,16 +1,47 @@
 package app.foodapp.model;
 
+import java.io.*;
+
 public enum MeasureSystem {
     US(0),
     METRIC(1);
 
-    private final int index;
-
-    private MeasureSystem (int index) {
-        this.index = index;
+    MeasureSystem(int index) {
     }
 
-    public void setMeasureSystem (MeasureSystem system) {
+    /*
+    This function returns the current measure system.
+     */
+    public static MeasureSystem getMeasureSystem() throws FileNotFoundException, IOException {
+        FileReader fileReader = new FileReader("preferences/MeasureSystem.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String content = bufferedReader.readLine();
+        bufferedReader.close();
+        switch (content) {
+            case "0": return US;
+            case "1": return METRIC;
+            default: throw new IOException("File's content doesn't match any Measure System");
+        }
+    }
 
+    /*
+    This function set the measure system of the ingredient. The system is saved in a file.
+     */
+    public static void setMeasureSystem (MeasureSystem measureSystem) throws IOException {
+        FileWriter fileWriter = new FileWriter("preferences/MeasureSystem.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        switch (measureSystem) {
+            case US:
+                bufferedWriter.write("0");
+                bufferedWriter.close();
+                break;
+            case METRIC:
+                bufferedWriter.write("1");
+                bufferedWriter.close();
+                break;
+            default:
+                bufferedWriter.close();
+                throw  new IOException("Measure system provided is not accepted.");
+        }
     }
 }
---
+++ src/test/java/app/foodapp/model/RecipeTest.java
@@ -0,0 +1,14 @@
+package app.foodapp.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RecipeTest {
+
+    @Test
+    public void testGetSteps() {
+    }
+}
