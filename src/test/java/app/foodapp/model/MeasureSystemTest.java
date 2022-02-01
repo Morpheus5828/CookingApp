137bfb03c76a4a8ddfdf2ec7a5b0dbb2d8f5547c
ChloeButtigieg <chloe.buttigieg12@gmail.com>
2022-02-01 11:30:14 +0100

    add class test MeasureSystemTest

---
+++ src/test/java/app/foodapp/model/MeasureSystemTest.java
@@ -0,0 +1,12 @@
+package app.foodapp.model;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MeasureSystemTest {
+}
