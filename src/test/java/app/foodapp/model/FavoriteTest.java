288fc2f28d0d029d7950d804c0e5233091900c05
HadrienStop <hadrien.stoppolani@etu.univ-amu.fr>
2022-02-05 18:37:30 +0100

    modify testAddToFavorites according to boolean return change

--- src/test/java/app/foodapp/model/FavoriteTest.java
+++ src/test/java/app/foodapp/model/FavoriteTest.java
@@ -10,19 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FavoriteTest {
 
+    Favorite testFavorite = new Favorite();
+    Recipe testRecipe1 = new Recipe(1620, null, null,8,30);
+    Recipe testRecipe2 = new Recipe(1400, null, null, 9, 45);
+
     @Test
     void testAddToFavorite() throws InstanceAlreadyExistsException {
-        Favorite testFavorite = new Favorite();
-        Recipe testRecipe1 = new Recipe(1620, null, null,8,30);
-        Recipe testRecipe2 = new Recipe(1400, null, null, 9, 45);
-
-        testFavorite.addToFavorite(testRecipe1);
-        testFavorite.addToFavorite(testRecipe2);
-        assertEquals(testFavorite.getFavorites(), List.of(testRecipe1, testRecipe2));
+        assertEquals(testFavorite.addToFavorite(testRecipe1), true);
+        assertEquals( testFavorite.addToFavorite(testRecipe2), true);
 
         assertThrows(InstanceAlreadyExistsException.class, () -> {
-           testFavorite.addToFavorite(testRecipe1);
+            testFavorite.addToFavorite(testRecipe1);
         });
     }
+
+    @Test
+    void testGetFavorite(){
+        assertEquals(testFavorite.getFavorites(), List.of(testRecipe1, testRecipe2));
+    }
+
 
 }
