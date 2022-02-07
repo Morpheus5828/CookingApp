11225757ed887110f1617957de637fa8e70b3bd4
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-02-07 10:05:58 +0100

    End of askMainChoice() method configuration in InteractionCLI class

--- src/main/java/app/foodapp/view/InteractionCLI.java
+++ src/main/java/app/foodapp/view/InteractionCLI.java
@@ -9,22 +9,34 @@ import java.util.Scanner;
 public class InteractionCLI {
 
     private ArrayList<String> listOfIngredient;
+    private Scanner terminalScanner;
 
     public InteractionCLI() {
         this.listOfIngredient = new ArrayList<>();
-        System.out.println("Hello World");
-        System.out.println("Enter ingredient(s):");
+        System.out.println("Hello World !\n");
 
-        askIngredient();
+        askMainChoice();
+        //askIngredient();
 
         System.out.println(this.listOfIngredient);
     }
 
+    public void askMainChoice() {
+        System.out.println("What do you want to see ?\n");
+        terminalScanner = new Scanner(System.in);
+        System.out.println("1\tRecipe research");
+        System.out.println("2\tFavorite list");
+        System.out.println("3\tSystem measures");
+        int answer = terminalScanner.nextInt();
+        System.out.println(answer);
+    }
+
     public void askIngredient() {
+        System.out.println("Enter ingredient(s):");
         boolean addIngredient = true;
         while(addIngredient) {
-            Scanner sc = new Scanner(System.in);
-            String ingredient = sc.nextLine();
+            terminalScanner = new Scanner(System.in);
+            String ingredient = terminalScanner.nextLine();
             if(Objects.equals(ingredient, "fin"))
                 addIngredient = false;
             else
