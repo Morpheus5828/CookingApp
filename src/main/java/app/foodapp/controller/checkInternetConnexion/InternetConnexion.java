dbba5f14a9cdb2653efcbf31a8c262d72aab2af2
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-02-17 18:38:14 +0100

    Configuration of checkStatus static method

--- src/main/java/app/foodapp/controller/checkInternetConnexion/InternetConnexion.java
+++ src/main/java/app/foodapp/controller/checkInternetConnexion/InternetConnexion.java
@@ -1,14 +1,22 @@
 package app.foodapp.controller.checkInternetConnexion;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class InternetConnexion {
     public static void checkStatus() throws IOException {
-        URL url = new URL("http://www.google.com");
-        URLConnection connection = url.openConnection();
-        System.out.println("Internet is connected" + "\n");
+        try {
+            URL url = new URL("http://www.google.com");
+            URLConnection connection = url.openConnection();
+            System.out.println("Internet is connected" + "\n");
+        } catch (MalformedURLException e) {
+            System.out.println(
+                "You are not connected to internet " + "\n" +
+                "Please, try again"
+            );
+        }
 
     }
 }
