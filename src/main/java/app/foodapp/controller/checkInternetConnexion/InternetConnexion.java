f08eb98f5e81f2072b9e6910aace52dcfc3427b2
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-02-17 18:33:25 +0100

    Creation of InternetConnection class

---
+++ src/main/java/app/foodapp/controller/checkInternetConnexion/InternetConnexion.java
@@ -0,0 +1,14 @@
+package app.foodapp.controller.checkInternetConnexion;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class InternetConnexion {
+    public static void checkStatus() throws IOException {
+        URL url = new URL("http://www.google.com");
+        URLConnection connection = url.openConnection();
+        System.out.println("Internet is connected" + "\n");
+
+    }
+}
