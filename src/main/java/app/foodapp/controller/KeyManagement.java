294975423d494c59ad69cc086701f1efed770f0d
chloe <chloe.buttigieg12@gmail.com>
2022-02-25 12:57:17 +0100

    Add method findNextKey() to class KeyManagement

--- src/main/java/app/foodapp/controller/KeyManagement.java
+++ src/main/java/app/foodapp/controller/KeyManagement.java
@@ -3,32 +3,55 @@ package app.foodapp.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class KeyManagement {
     private static int keyIndex = 0;
-    private boolean isKeyBeforeValid = true;
+    private static boolean isKeyBeforeValid = true;
+    private static boolean waitingForKeyToBeValid = false;
+
+    public static String getKey() {
+        String key = null;
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/dataBase/keys.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            for (int index = 0; index < keyIndex; index++) {
+                bufferedReader.readLine();
+            }
+            key = bufferedReader.readLine();
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return key;
+    }
 
     public static String getNextKey() {
+        return null;
+    }
+
+    private static String findNextKey() {
+        String key = null;
         try {
             FileReader fileReader = new FileReader("src/main/resources/dataBase/keys.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            for (int index = 0; index < keyIndex; index++) {
+            for (int index = 0; index <= keyIndex; index++) {
                 bufferedReader.readLine();
             }
             keyIndex++;
-            String key = bufferedReader.readLine();
+            key = bufferedReader.readLine();
             bufferedReader.close();
 
             if (key == null) {
-                bufferedReader = new BufferedReader(fileReader);
                 keyIndex = 0;
-                return bufferedReader.readLine();
+                bufferedReader = new BufferedReader(fileReader);
+                key = bufferedReader.readLine();
+                bufferedReader.close();
             }
-            return key;
 
         } catch (IOException e) {e.printStackTrace();}
-        return null;
+        return key;
     }
+
 }
