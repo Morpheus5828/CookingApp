b182d7209b895fda4cb2e4bf8886305f73d4d206
chloe <chloe.buttigieg12@gmail.com>
2022-02-26 21:44:18 +0100

    Modify testCheckForDataExtraction test method

--- src/test/java/app/foodapp/controller/dataRequest/MainInstructionsRequestTest.java
+++ src/test/java/app/foodapp/controller/dataRequest/MainInstructionsRequestTest.java
@@ -2,17 +2,14 @@ package app.foodapp.controller.dataRequest;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainInstructionsRequestTest {
     @Test
     public void testCheckForDataExtraction() {
-        String ingredientExample = "apples,+sugar&number=2";
-        MainInstructionsRequest request = new MainInstructionsRequest(ingredientExample);
+        MainInstructionsRequest request = new MainInstructionsRequest("622561");
         int statusCode = request.getStatusCode();
-        // ths test can fail if user does more 100 request during 24h
-        assertEquals(statusCode, 200);
+        assertEquals(200, statusCode);
 
     }
 }
