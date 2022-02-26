c87ff47a6532245e0c6f82a05c444c3e917982aa
hadrien <hadrien.stoppolani@etu.univ-amu.fr>
2022-02-26 16:36:19 +0100

    add methods to remove ingredients and to display ingredients in Controller

--- src/main/java/app/foodapp/controller/Controller.java
+++ src/main/java/app/foodapp/controller/Controller.java
@@ -6,15 +6,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextFlow;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.LinkedList;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller implements Initializable {
 
@@ -26,6 +27,10 @@ public class Controller implements Initializable {
     @FXML private Button fourthIngredient;
     @FXML private Button fifthIngredient;
     @FXML private Button sixthIngredient;
+    @FXML private Button submitButton;
+    @FXML private Label mainDisplay;
+
+    private ArrayList<String> strings = new ArrayList<String>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
@@ -48,26 +53,32 @@ public class Controller implements Initializable {
                     if(!sixthIngredient.isVisible()){
                         sixthIngredient.setText(searchByIngredient.getText() + "  x");
                         sixthIngredient.setVisible(true);
+                        strings.add(0, searchByIngredient.getText());
                     }
                     else if(sixthIngredient.isVisible() && !fifthIngredient.isVisible()){
                         fifthIngredient.setText(searchByIngredient.getText() + "  x");
                         fifthIngredient.setVisible(true);
+                        strings.add(1, searchByIngredient.getText());
                     }
                     else if(fifthIngredient.isVisible() && !fourthIngredient.isVisible()){
                         fourthIngredient.setText(searchByIngredient.getText() + " x");
                         fourthIngredient.setVisible(true);
+                        strings.add(2, searchByIngredient.getText());
                     }
                     else if(fourthIngredient.isVisible() && !thirdIngredient.isVisible()){
                         thirdIngredient.setText(searchByIngredient.getText() + " x");
                         thirdIngredient.setVisible(true);
+                        strings.add(3, searchByIngredient.getText());
                     }
                     else if(thirdIngredient.isVisible() && !secondIngredient.isVisible()){
                         secondIngredient.setText(searchByIngredient.getText() + " x");
                         secondIngredient.setVisible(true);
+                        strings.add(4, searchByIngredient.getText());
                     }
                     else if(secondIngredient.isVisible() && !firstIngredient.isVisible()){
                         firstIngredient.setText(searchByIngredient.getText() + " x");
                         firstIngredient.setVisible(true);
+                        strings.add(5, searchByIngredient.getText());
                     }
                     searchByIngredient.clear();
                 }
@@ -75,8 +86,43 @@ public class Controller implements Initializable {
         });
     }
 
+    public void removeFirstIngredient(ActionEvent actionEvent) {
+        strings.remove(5);
+        firstIngredient.setVisible(false);
+        firstIngredient.setText("");
+    }
 
-    public void removeIngredient(ActionEvent actionEvent) {
-        
+    public void removeSecondIngredient(ActionEvent actionEvent) {
+        strings.remove(4);
+        secondIngredient.setVisible(false);
+        secondIngredient.setText("");
+    }
+
+    public void removeThirdIngredient(ActionEvent actionEvent) {
+        strings.remove(3);
+        thirdIngredient.setVisible(false);
+        thirdIngredient.setText("");
+    }
+
+    public void removeFourthIngredient(ActionEvent actionEvent) {
+        strings.remove(2);
+        fourthIngredient.setVisible(false);
+        fourthIngredient.setText("");
+    }
+
+    public void removeFifthIngredient(ActionEvent actionEvent) {
+        strings.remove(1);
+        fifthIngredient.setVisible(false);
+        fifthIngredient.setText("");
+    }
+
+    public void removeSixthIngredient(ActionEvent actionEvent) {
+        strings.remove(0);
+        sixthIngredient.setVisible(false);
+        sixthIngredient.setText("");
+    }
+
+    public void displayApiInformations(ActionEvent actionEvent) {
+        mainDisplay.setText(strings.toString());
     }
 }
