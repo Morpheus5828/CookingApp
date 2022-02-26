9603ce38f0e7141776c6969d42dc195a623b6ea1
chloe <chloe.buttigieg12@gmail.com>
2022-02-26 21:44:18 +0100

    Modify testCheckForDataExtraction test method

--- src/main/java/app/foodapp/view/FoodApp.java
+++ src/main/java/app/foodapp/view/FoodApp.java
@@ -12,13 +12,10 @@ public class FoodApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        /*Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/foodapp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/foodapp.fxml"));
         primaryStage.setTitle("App");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-         */
-        Recipe recipe = new Recipe("622561", null, 0, 0);
-        System.out.println(recipe.getSteps());
     }
 
     public static void main(String[] args) { launch(args); }
