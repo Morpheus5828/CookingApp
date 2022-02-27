495987030e9474fc3efd0990510de0317cf7f191
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-02-27 14:42:19 +0100

    Login system functionned with quite bhug

--- src/main/java/app/foodapp/controller/login/LoginPage.java
+++ src/main/java/app/foodapp/controller/login/LoginPage.java
@@ -1,59 +1,69 @@
 package app.foodapp.controller.login;
 
+import app.foodapp.view.FoodApp;
 import app.foodapp.view.alert.AlertFound;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 import java.io.*;
 
-public final class LoginPage {
+public class LoginPage {
     @FXML private TextField usernameEntered;
     @FXML private PasswordField passwordEntered;
-    private BufferedReader reader;
+    private BufferedReader reader = null;
+    private PrintWriter wrote = null;
+    private String userInformation = "userInformation.txt";
+    private String line = "";
+
+
+
+    public LoginPage() throws IOException {
+        this.reader = new BufferedReader(new FileReader(userInformation));
+    }
 
 
     public void checkUserLogin(ActionEvent actionEvent) throws IOException {
+        boolean isTheSameUsername = false;
+        boolean isTheSamePassword = false;
         try {
-            reader = new BufferedReader(new FileReader("userInformation.txt"));
-            boolean isTheSameUsername = false;
-            boolean isTheSamePassword = false;
-            String line = "";
             while ((line = reader.readLine()) != null) {
-                int counter = 0;
-                String[] tabOfRow = line.split(",");
-
-                for (String index : tabOfRow) {
-                    if (counter != 2) {
+                int compteur = 0;
+                String[] row = line.split(",");
+                for(String index : row) {
+                    if(compteur == 0) {
                         if (index.equals(usernameEntered.getText()))
                             isTheSameUsername = true;
-                        if (index.equals(passwordEntered.getText()))
+                        if(index.equals(passwordEntered.getText()))
                             isTheSamePassword = true;
                     }
-                    counter += 1;
+                    compteur += 1;
                 }
             }
-
             if(!isTheSameUsername)
                 AlertFound.usernameNotExist();
-            if(!isTheSamePassword)
+            else if(!isTheSamePassword)
                 AlertFound.passwordNotExist();
             if(isTheSamePassword && isTheSameUsername)
-                // Open next stage
                 loginAccepted();
-        }
-
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            reader.close();
+        }
+        if(isTheSameUsername)
+            loginAccepted();
     }
 
-    @FXML
-    private void creationOfAnAccount(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void creationOfAnAccount(javafx.event.ActionEvent actionEvent) throws IOException {
         Stage loginStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/register/sign_up.fxml"));
         loginStage.setTitle("Cooking App");
@@ -61,7 +71,7 @@ public final class LoginPage {
         loginStage.show();
     }
 
-    private void loginAccepted() throws IOException {
+    public void loginAccepted() throws IOException {
         Stage loginStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/favorites.fxml"));
         loginStage.setTitle("Cooking App");
