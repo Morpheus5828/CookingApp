55c8956c2d6df6dc9df745e329520ed52bc4b85c
chloe <chloe.buttigieg12@gmail.com>
2022-03-01 20:49:47 +0100

    add css style to favorites.css

--- src/main/java/app/foodapp/model/dataManipulation/recipe/FavoriteStamp.java
+++ src/main/java/app/foodapp/model/dataManipulation/recipe/FavoriteStamp.java
@@ -8,7 +8,7 @@ public class FavoriteStamp {
     ArrayList<Recipe> favorites = new ArrayList<>();
 
     public FavoriteStamp() {
-        favorites.add(new Recipe("73420", "apple recipe", 3, 4));
+        favorites.add(new Recipe("73420", "apple", 3, 4));
         favorites.add(new Recipe("632660", "banana recipe", 2, 40));
         favorites.add(new Recipe("9003", "apple recipe", 3, 4));
     }
