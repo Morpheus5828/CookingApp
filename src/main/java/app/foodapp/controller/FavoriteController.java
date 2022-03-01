e434e5cba25e3279b66be56c8777c47d928ca9da
chloe <chloe.buttigieg12@gmail.com>
2022-03-01 14:10:43 +0100

    Add method goToMenu

--- src/main/java/app/foodapp/controller/FavoriteController.java
+++ src/main/java/app/foodapp/controller/FavoriteController.java
@@ -1,32 +1,41 @@
 package app.foodapp.controller;
 
+import app.foodapp.model.dataManipulation.recipe.FavoriteStamp;
 import app.foodapp.model.dataManipulation.recipe.Recipe;
 import app.foodapp.model.node.Favorite;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class FavoriteController implements Initializable {
 
     @FXML private AnchorPane centerPane;
+    @FXML private AnchorPane rootPane;
 
-    Favorite favoriteNode = new Favorite();
+    FavoriteStamp favoriteNode = new FavoriteStamp();
     ArrayList<Button> buttons = new ArrayList<>();
     ArrayList<HBox> contents = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
 
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/app/foodapp/view/foodapp.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
     public void showFavorites() {
-        List<Recipe> favorites = favoriteNode.getFavoriteList();
+        ArrayList<Recipe> favorites = favoriteNode.getFavorites();
         for (Recipe recipe : favorites) {
             HBox content = new HBox();
             contents.add(content);
