0e5af55125923db6bee926e09af1bc91ceae6f8d
chloe <chloe.buttigieg12@gmail.com>
2022-03-01 12:49:20 +0100

    add constructor and method initialize to class FavoriteController

--- src/main/java/app/foodapp/controller/FavoriteController.java
+++ src/main/java/app/foodapp/controller/FavoriteController.java
@@ -1,4 +1,21 @@
 package app.foodapp.controller;
 
-public class FavoriteController {
+import app.foodapp.model.node.Favorite;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FavoriteController implements Initializable {
+
+    public FavoriteController() {
+        showFavorites();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+    public void showFavorites() {
+
+    }
 }
