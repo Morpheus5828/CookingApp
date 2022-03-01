7fb676dadffab7da610b60f3d54ea28e9e8d89e8
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-03-01 16:20:24 +0100

    Add condition to InteractionCLI to internetStatus

--- src/main/java/app/foodapp/view/InteractionCLI.java
+++ src/main/java/app/foodapp/view/InteractionCLI.java
@@ -1,17 +1,19 @@
 package app.foodapp.view;
 
+import app.foodapp.controller.checkInternetConnexion.InternetConnexion;
 import app.foodapp.model.dataManipulation.recipe.Recipe;
 import app.foodapp.model.dataManipulation.recipe.RecipeInformation;
 import app.foodapp.model.node.GetRecipeByIngredient;
 import app.foodapp.model.node.Pane;
 import app.foodapp.model.node.RecipeDetails;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class InteractionCLI {
-    public InteractionCLI()  {
-       // TODO Check internet connexion before begin
-       new Pane();
+    public InteractionCLI() throws IOException {
+        if(InternetConnexion.checkStatus())
+            new Pane();
     }
 
 
