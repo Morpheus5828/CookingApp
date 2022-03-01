f43fa0ac2ba3cfe5a3e14c3265cdad7fc6309303
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-03-01 14:56:40 +0100

    SignUpTest done

--- src/main/java/app/foodapp/view/FoodApp.java
+++ src/main/java/app/foodapp/view/FoodApp.java
@@ -1,5 +1,6 @@
 package app.foodapp.view;
 
+import app.foodapp.controller.login.SignUp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,7 +16,7 @@ public class FoodApp extends Application {
            Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/register/login.fxml"));
            loginStage.setTitle("Cooking App");
            loginStage.setScene(new Scene(root));
-           loginStage.show();
+           //loginStage.show();
 
        } catch (Exception e) {
            e.printStackTrace();
