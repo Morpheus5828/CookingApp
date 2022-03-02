6e40a5ac7fd0713fb98457465f96bc82975216be
chloe <chloe.buttigieg12@gmail.com>
2022-03-02 14:05:05 +0100

    modify display of recipe

--- src/main/java/app/foodapp/model/dataManipulation/recipe/FavoriteStamp.java
+++ src/main/java/app/foodapp/model/dataManipulation/recipe/FavoriteStamp.java
@@ -8,9 +8,9 @@ public class FavoriteStamp {
     ArrayList<Recipe> favorites = new ArrayList<>();
 
     public FavoriteStamp() {
-        favorites.add(new Recipe("73420", "apple", 3, 4));
+        favorites.add(new Recipe("73420", "apple", 30, 4));
         favorites.add(new Recipe("632660", "banana recipe", 2, 40));
-        favorites.add(new Recipe("9003", "apple recipe", 3, 4));
+        favorites.add(new Recipe("9003", "apple recipe", 3, 300));
     }
 
     public void removeFromFavorite(Recipe recipe) {
