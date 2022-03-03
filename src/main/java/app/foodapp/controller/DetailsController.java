f492648026f362ce2679a216da58c71508495f8b
HadrienStop <hadrien.stoppolani@etu.univ-amu.fr>
2022-03-03 13:19:52 +0100

    add method display

--- src/main/java/app/foodapp/controller/DetailsController.java
+++ src/main/java/app/foodapp/controller/DetailsController.java
@@ -1,11 +1,20 @@
 package app.foodapp.controller;
 
 import app.foodapp.model.dataManipulation.recipe.Recipe;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class DetailsController {
+    @FXML private Text detailsDisplay;
+
     public void showDetails(Recipe recipe) {
         String steps = recipe.getSteps();
         System.out.println(recipe.getTitle());
         System.out.println(steps);
+        display(recipe);
+    }
+
+    private void display(Recipe recipe){
+        detailsDisplay.setText(recipe.displayDetailsCharacteristicsGUI());
     }
 }
