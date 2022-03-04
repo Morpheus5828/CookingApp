ac8d1091bca1733f423e09265dd299981392ad44
HadrienStop <hadrien.stoppolani@etu.univ-amu.fr>
2022-03-04 23:54:51 +0100

    rebuild display system and design

--- src/main/java/app/foodapp/controller/Controller.java
+++ src/main/java/app/foodapp/controller/Controller.java
@@ -3,6 +3,7 @@ package app.foodapp.controller;
 import app.foodapp.model.dataManipulation.recipe.Recipe;
 import app.foodapp.model.dataManipulation.recipe.RecipeInformation;
 import app.foodapp.model.node.Favorite;
+import com.google.gson.JsonArray;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -12,10 +13,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -26,9 +33,10 @@ import java.util.*;
 public class Controller implements Initializable {
 
     @FXML private TextField searchByIngredient;
-    @FXML private Text mainDisplay;
-    @FXML private AnchorPane buttonsAnchorPane;
+    //@FXML private Text mainDisplay;
+    //@FXML private AnchorPane buttonsAnchorPane;
     @FXML private AnchorPane ingredientsAnchorPane;
+    @FXML private VBox recipeDisplay;
 
 
     private ArrayList<Button> ingredientButtons = new ArrayList<Button>();
@@ -38,6 +46,7 @@ public class Controller implements Initializable {
     protected Favorite favorites = new Favorite();
     private ArrayList<Button> detailsButtons = new ArrayList<Button>();
     protected Recipe recipeSelectedForDetails;
+    private final ArrayList<HBox> recipeBoxDisplayList = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
@@ -73,17 +82,133 @@ public class Controller implements Initializable {
     private boolean isSearchLunched = false;
 
     public void displayApiInformations(ActionEvent actionEvent) {
-        buttonsAnchorPane.getChildren().clear();
+        //buttonsAnchorPane.getChildren().clear();
+        recipeDisplay.getChildren().clear();
         favoritesButtons.clear();
         detailsButtons.clear();
+        recipeBoxDisplayList.clear();
         recipeInformation = new RecipeInformation(strings);
-        mainDisplay.setText(recipeInformation.displayGUI());
-        createFavoriteButtons();
-        createDetailsButtons();
+
+        //mainDisplay.setText(recipeInformation.displayGUI());
+
+        for (Recipe recipe : recipeInformation.listOfRecipe) {
+            HBox recipeBoxDisplay = new HBox();
+            recipeBoxDisplay.getStyleClass().add("recipe-content");
+            recipeBoxDisplayList.add(recipeBoxDisplay);
+
+            Label title = createLabel(recipe.getTitle(), "recipe-title");
+            Label cookingTime = createLabel((int) Math.round(recipe.getCookingTime()) + " min", "recipe-cookingTime");
+            Label servings = createLabel((int) Math.round(recipe.getServings()) + " servings", "recipe-servings");
+
+            ImageView removeFromFavoriteImage = new ImageView(new Image(getClass().getResourceAsStream("/app/foodapp/view/images/picturesForFavorites/full-heart.png")));
+            removeFromFavoriteImage.setPreserveRatio(true);
+            removeFromFavoriteImage.setFitWidth(30);
+
+            Button removeFromFavoriteButton = new Button("", removeFromFavoriteImage);
+            removeFromFavoriteButton.getStyleClass().add("button-favorite");
+
+
+            removeFromFavoriteButton.addEventFilter(MouseEvent.MOUSE_ENTERED, setBrokenHeartImage(removeFromFavoriteImage));
+            removeFromFavoriteButton.addEventFilter(MouseEvent.MOUSE_EXITED, setFullHeartImage(removeFromFavoriteImage));
+            recipeBoxDisplay.addEventFilter(MouseEvent.MOUSE_CLICKED, getRecipeDetails(recipe));
+            recipeBoxDisplay.addEventFilter(MouseEvent.MOUSE_ENTERED, mouseEnteredRecipeBoxDisplay(recipeBoxDisplay));
+            recipeBoxDisplay.addEventFilter(MouseEvent.MOUSE_EXITED, mouseExitedRecipeBoxDisplay(recipeBoxDisplay));
+
+            recipeBoxDisplay.getChildren().add(title);
+            recipeBoxDisplay.getChildren().add(cookingTime);
+            recipeBoxDisplay.getChildren().add(servings);
+            recipeBoxDisplay.getChildren().add(removeFromFavoriteButton);
+            recipeDisplay.getChildren().add(recipeBoxDisplay);
+        }
+
+        //createFavoriteButtons();
+        //createDetailsButtons();
         isSearchLunched = true;
     }
 
-    private void createFavoriteButtons(){
+    public Label createLabel(String content, String styleClass) {
+        Label label = new Label(content);
+        label.getStyleClass().add(styleClass);
+        return label;
+    }
+
+    public EventHandler<MouseEvent> setBrokenHeartImage(ImageView imageView) {
+        return new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                imageView.setImage(new Image(getClass().getResourceAsStream("/app/foodapp/view/images/picturesForFavorites/broken-heart.png")));
+            }
+        };
+    }
+
+    public EventHandler<MouseEvent> setFullHeartImage(ImageView imageView) {
+        return new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                imageView.setImage(new Image(getClass().getResourceAsStream("/app/foodapp/view/images/picturesForFavorites/full-heart.png")));
+            }
+        };
+    }
+
+    public EventHandler<MouseEvent> getRecipeDetails(Recipe recipe) {
+        return new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                goToRecipeDetails(event, recipe);
+            }
+        };
+    }
+
+    public void goToRecipeDetails(MouseEvent event, Recipe recipe) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/foodapp/view/details.fxml"));
+            Parent root = loader.load();
+            DetailsController detailsController = loader.getController();
+            detailsController.showDetails(recipe);
+
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("/app/foodapp/view/details.css").toExternalForm();
+
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public EventHandler<MouseEvent> mouseEnteredRecipeBoxDisplay(HBox recipeBoxDisplay) {
+        return new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Label title = (Label) recipeBoxDisplay.getChildren().get(0);
+                Label cookingTime = (Label) recipeBoxDisplay.getChildren().get(1);
+                Label servings = (Label) recipeBoxDisplay.getChildren().get(2);
+
+                recipeBoxDisplay.getStyleClass().add("recipe-content-hover");
+                cookingTime.getStyleClass().add("recipe-information-hover");
+                servings.getStyleClass().add("recipe-information-hover");
+            }
+        };
+    }
+
+    public EventHandler<MouseEvent> mouseExitedRecipeBoxDisplay(HBox recipeBoxDisplay) {
+        return new EventHandler<>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Label title = (Label) recipeBoxDisplay.getChildren().get(0);
+                Label cookingTime = (Label) recipeBoxDisplay.getChildren().get(1);
+                Label servings = (Label) recipeBoxDisplay.getChildren().get(2);
+
+                recipeBoxDisplay.getStyleClass().remove("recipe-content-hover");
+                cookingTime.getStyleClass().remove("recipe-information-hover");
+                servings.getStyleClass().remove("recipe-information-hover");
+            }
+        };
+    }
+
+        private void createFavoriteButtons(){
         int positionY = 20;
         for(int index = 0 ; index != recipeInformation.listOfRecipe.size() ; index++){
             Button newFavoriteButton = new Button();
@@ -91,7 +216,7 @@ public class Controller implements Initializable {
             newFavoriteButton.setText("<3");
             newFavoriteButton.setLayoutX(0);
             newFavoriteButton.setLayoutY(positionY);
-            buttonsAnchorPane.getChildren().add(newFavoriteButton);
+            //buttonsAnchorPane.getChildren().add(newFavoriteButton);
             favoritesButtons.add(newFavoriteButton);
             newFavoriteButton.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
@@ -161,7 +286,7 @@ public class Controller implements Initializable {
             newDetailsButton.setText("Details");
             newDetailsButton.setLayoutX(80);
             newDetailsButton.setLayoutY(positionY);
-            buttonsAnchorPane.getChildren().add(newDetailsButton);
+            //buttonsAnchorPane.getChildren().add(newDetailsButton);
             detailsButtons.add(newDetailsButton);
             newDetailsButton.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
