2d2c4176a3699265c625cdb690f92cae98ebb445
chloe <chloe.buttigieg12@gmail.com>
2022-03-06 20:49:51 +0100

    change name of FoodApp.java into FoodAppMain.java

--- src/main/java/app/foodapp/view/FoodAppMain.java
+++ src/main/java/app/foodapp/view/FoodAppMain.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class FoodApp extends Application {
+public class FoodAppMain extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
