09cbcd961460baa8c98a5dc52d70722345ac96f8
Marius THORRE <marius.thorre@etu.univ-amu.fr>
2022-03-07 19:43:44 +0100

    Background

--- src/main/java/app/foodapp/view/FoodAppCLI.java
+++ src/main/java/app/foodapp/view/FoodAppCLI.java
@@ -1,23 +1,18 @@
 package app.foodapp.view;
 
 import app.foodapp.controller.checkInternetConnexion.InternetConnexion;
-import app.foodapp.model.node.Favorite;
-import app.foodapp.model.node.GetRecipeByIngredient;
 import app.foodapp.model.node.Pane;
-import app.foodapp.model.node.login.LoginPage;
-import app.foodapp.model.node.login.SignUp;
-import app.foodapp.model.recipe.Recipe;
-import app.foodapp.model.recipe.RecipeInformation;
+
 
 import java.io.IOException;
 
 public class FoodAppCLI {
 
     public static void main(String[] args) throws IOException {
-        /*if (InternetConnexion.checkStatus())
-            new Pane();*/
-        GetRecipeByIngredient test = new GetRecipeByIngredient();
-        test.launch();
+        if (InternetConnexion.checkStatus())
+            new Pane();
+        //GetRecipeByIngredient test = new GetRecipeByIngredient();
+        //test.launch();
 
 
     }
